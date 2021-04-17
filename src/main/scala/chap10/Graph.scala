aa5fc04ddd257e09409786076878ef05edf82a7f
hiramekun <thescript1210@gmail.com>
2021-04-17 23:42:51 +0900

    Add simple graph

---
+++ src/main/scala/chap10/Graph.scala
@@ -0,0 +1,12 @@
+package chap10
+
+
+case class Edge(to: Int, w: Long)
+
+case class Graph(n: Int) {
+  val g: Array[List[Edge]] = Array.fill(n)(List.empty)
+
+  def push(from: Int, edge: Edge): Unit = {
+    g(from) ::= edge
+  }
+}
