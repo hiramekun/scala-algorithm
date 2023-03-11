c089115d42108e28b8b8ff360f64c10f50206081
hiramekun <thescript1210@gmail.com>
2023-03-11 13:04:07 +0900

    fix

--- src/main/scala/leetcode/GroupAnagrams.scala
+++ src/main/scala/leetcode/GroupAnagrams.scala
@@ -2,8 +2,6 @@ package leetcode
 
 object Solution {
   def groupAnagrams(strs: Array[String]): List[List[String]] = {
-    strs.toList.zipWithIndex.map {
-      case (s, i) => s.toSeq.sorted.unwrap -> i
-    }.groupBy(_._1).values.map(_.map { case (_, i) => strs(i) }).toList
+    strs.toList.groupBy(_.sorted).values.toList
   }
 }
