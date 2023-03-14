bf93515facdd5b8e6210ecad0e34dd5ba9285227
hiramekun <thescript1210@gmail.com>
2023-03-14 10:47:35 +0900

    speed up

--- src/main/scala/leetcode/ProductOfArrayExceptSelf.scala
+++ src/main/scala/leetcode/ProductOfArrayExceptSelf.scala
@@ -3,17 +3,16 @@ package leetcode
 object Solution {
   def productExceptSelf(nums: Array[Int]): Array[Int] = {
     val n = nums.sizeIs
-    val is = nums.indices
-    val leftProducts = new Array[Int](n + 1)
-    val rightProducts = new Array[Int](n + 1)
-    leftProducts(0) = 1
-    rightProducts(0) = 1
-
-    for (i <- is) {
-      leftProducts(i + 1) = leftProducts(i) * nums(i)
-      rightProducts(i + 1) = rightProducts(i) * nums(n - i - 1)
+    val result = new Array[Int](n)
+    result(0) = 1
+    for (i <- 0 until n - 1) {
+      result(i + 1) = result(i) * nums(i)
     }
-
-    is.map(i => leftProducts(i) * rightProducts(n - i - 1)).toArray
+    var rightProducts = 1
+    for (i <- 0 until n) {
+      result(n - i - 1) *= rightProducts
+      rightProducts *= nums(n - i - 1)
+    }
+    result
   }
 }
