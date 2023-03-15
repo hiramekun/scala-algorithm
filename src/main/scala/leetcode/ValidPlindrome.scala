e4e52f56a5581ff1ade89eab1cdd62f53958b654
hiramekun <thescript1210@gmail.com>
2023-03-15 23:04:33 +0900

    enhance

--- src/main/scala/leetcode/ValidPlindrome.scala
+++ src/main/scala/leetcode/ValidPlindrome.scala
@@ -1,9 +1,8 @@
 package leetcode
 
-
 object Solution {
   def isPalindrome(s: String): Boolean = {
-    val s2 = s.filter(c => 'a' <= c && c <= 'z' || 'A' <= c && c <= 'Z' || '0' <= c && c <= '9').map(_.toLower)
+    val s2 = s.filter(_.isLetterOrDigit).toLowerCase
     s2 == s2.reverse
   }
 }
