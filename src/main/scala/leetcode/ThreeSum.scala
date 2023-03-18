a23a16f9df0e6f34448fd8b1b49c55222bc82014
hiramekun <thescript1210@gmail.com>
2023-03-19 02:03:49 +0900

    ThreeSum

---
+++ src/main/scala/leetcode/ThreeSum.scala
@@ -0,0 +1,17 @@
+package leetcode
+
+import scala.collection.mutable
+
+object Solution {
+  def threeSum(nums: Array[Int]): List[List[Int]] = {
+    val map = nums.zipWithIndex.toMap
+    (
+      for(
+        i <- LazyList.range(0, nums.length - 2);
+        j <- LazyList.range(i+1, nums.length - 1);
+        total = nums(i) + nums(j);
+        l = List(nums(i), nums(j), -total).sorted if (map.contains(-total) && map(-total) != i && map(-total) != j)
+      ) yield l
+      ).distinct.toList
+  }
+}
